cb1b49f9f0d33225a56b09301c751c2cf3091c5e
Chard Peter <chardpeter10@gmail.com>
2024-11-13 21:37:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const bodyParser = require ("body-parser");
 const cors = require ("cors");
 
-const {Configuration, OpenAiApi } = require ("openai");
+// const {Configuration, OpenAiApi } = require ("openai");            openai is the api key location of the application
 
 const config = new Configuration ({
     apiKey: "#",
